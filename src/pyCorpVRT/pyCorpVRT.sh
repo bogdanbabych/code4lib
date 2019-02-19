4ebd2ddc43f8bdb56a76c492ac73049dbac986fc
Bogdan Babych <bogdan.babych@gmail.com>
2019-02-19 10:53:35 +0000

    s010x development

--- src/pyCorpVRT/pyCorpVRT.sh
+++ src/pyCorpVRT/pyCorpVRT.sh
@@ -1,4 +1,4 @@
 # python pyCorpVRT.py "LKeyColumns.extend([2,1])" "LValueColumns.extend([1])" < pyCorpVRT_in.txt > pyCorpVRT_out.txt
 # python pyCorpVRT.py "LKeyColumns.extend([2,1])" "LValueColumns.extend([1])" < pyCorpVRT_in.txt > pyCorpVRT_out.txt
 
-python pyCorpVRT.py "LKeyColumns.extend([0])" "LValueColumns.extend([1])" < pyCorpVRT_in_BNC1994-10k.vrt > pyCorpVRT_out_BNC1994-10k.txt
+python pyCorpVRT.py "LKeyColumns.extend([0])" "LValueColumns.extend([2,1])" < pyCorpVRT_in_BNC1994-10k.vrt > pyCorpVRT_out_BNC1994-10k.txt
