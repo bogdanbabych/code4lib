8e0a7119f37fa5abc60f33a4e6c905777ad5370b
Bogdan Babych <bogdan.babych@gmail.com>
2019-02-20 11:40:24 +0000

    s010x development

--- src/pyCorpVRT/pyCorpVRT.sh
+++ src/pyCorpVRT/pyCorpVRT.sh
@@ -5,6 +5,6 @@
 # python pyCorpVRT.py "LKeyColumns.extend([0])" "LValueColumns.extend([2,1])" "LFlags.extend(['fileVRT2dict','dict2dstr'])" < pyCorpVRT_in_canada2018.vrt > pyCorpVRT_out_canada2018.txt
 
 
-python pyCorpVRT.py "LKeyColumns.extend([2])" "LValueColumns.extend([1])" "LIndexColumns.extend([2,1])" "LFlags.extend(['fileVRT2posTemplates','templates2d'])" < pyCorpVRT_in_BNC1994-10k.vrt > pyCorpVRT_out_BNC1994-10k.txt
-python pyCorpVRT.py "LKeyColumns.extend([2])" "LValueColumns.extend([1])" "LIndexColumns.extend([2,1])" "LFlags.extend(['fileVRT2posTemplates','templates2d'])" < pyCorpVRT_in_canada2018.vrt > pyCorpVRT_out_canada2018.txt
+python pyCorpVRT.py "LKeyColumns.extend([2,1])" "LValueColumns.extend([1])" "LIndexColumns.extend([2,1])" "LFlags.extend(['fileVRT2posTemplates','templates2d'])" < pyCorpVRT_in_BNC1994-10k.vrt > pyCorpVRT_out_BNC1994-10k.txt
+python pyCorpVRT.py "LKeyColumns.extend([2,1])" "LValueColumns.extend([1])" "LIndexColumns.extend([2,1])" "LFlags.extend(['fileVRT2posTemplates','templates2d'])" < pyCorpVRT_in_canada2018.vrt > pyCorpVRT_out_canada2018.txt
 
